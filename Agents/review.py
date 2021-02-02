1543cc1d2fc58c5fae331643b81f538e1c41fc8b
KadeLehman <klehman@smu.edu>
2021-02-01 22:27:05 -0600

    quick spacing fix

--- Agents/review.py
+++ Agents/review.py
@@ -6,8 +6,8 @@ arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
 mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
+    'cars': ["BMW", "Volvo", "Ford"],
+    'passings': [3, 7, 2]
 }
 
 myvar = pd.DataFrame(mydataset)
